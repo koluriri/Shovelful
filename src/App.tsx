c0efacbe8e4e9d802a258142cdff6449e649abb9
onpp <10293030+onpp@users.noreply.github.com>
2023-04-15 00:58:06 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [pitch, setPitch] = useState(1);
+
+  const readAloud = () => {
+    if ("speechSynthesis" in window) {
+      const uttr = new SpeechSynthesisUtterance();
+      uttr.text = text;
+      uttr.lang = "ja-JP";
+      uttr.pitch = pitch;
+      window.speechSynthesis.speak(uttr);
+
+      return;
+    }
+    alert("大変申し訳ありません。このブラウザは音声合成に対応していません。");
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      readAloud();
+      setTimeout(() => {
+        setText("");
+      }, 100);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Shovelful</h1>
+      <p>
+        声の高さ
+        <input
+          type="range"
+          min={0}
+          max={2}
+          step={0.1}
+          value={pitch}
+          onChange={(e) => setPitch(Number(e.target.value))}
+        />
+      </p>
+      <input
+        type="text"
+        placeholder="入力…"
+        value={text}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+      <p>
+        音声を入力するとリアルタイムで話します。
+        <br />
+        ※エンターで発火
+      </p>
+    </>
   );
 }
 
