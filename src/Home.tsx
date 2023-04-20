24eed779477ad2069cd5a6e86cc81f2510e5d11e
onpp <10293030+onpp@users.noreply.github.com>
2023-04-20 23:23:05 +0900

    Homeをデザイン

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,46 @@
 import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "./logo.svg";
 
 const Home = () => {
   return (
     <>
-      <h1>ホーム</h1>
-      <div>
-        <Link to={`/speech/`}>しゃべる</Link>
+      <nav>
+        <h1>
+          <Logo />
+          Shovelful
+        </h1>
+      </nav>
+      <div className="hero">
+        <div className="illust">
+          <img src="/img/illust.svg" />
+        </div>
+        <div className="copy">
+          <h2>
+            声が出せないときでも
+            <br />
+            通話できる！
+          </h2>
+          <p className="lead">
+            操作はエンター（スマホの場合は改行）だけ。
+            <br />
+            <strong>リアルタイムな音声読み上げ</strong>により
+            <br />
+            <strong>普通の会話と同じ</strong>ようなスピードで通話ができる！
+          </p>
+          <small>
+            ※通話で使用するにはSoundflowerなどの仮想オーディオデバイスの設定が必要な場合があります。
+          </small>
+          <Link to={`/speech/`} className="speech-btn">
+            しゃべる→
+          </Link>
+        </div>
       </div>
+      <footer>
+        つくったひと:{" "}
+        <a href="//twitter.com/koluriri" target="_blank" rel="noreferrer">
+          @koluriri
+        </a>
+      </footer>
     </>
   );
 };
